e2e02f00526d6fd5cabe584899d3add59a525679
Andrey Fontoura <andrey.reis@ufba.br>
2021-10-12 18:40:07 -0300

    Display the binary time results with the red LEDs

--- BCD_counter.v
+++ BCD_counter.v
@@ -1,14 +1,24 @@
-module BCD_counter (Clock, Clear, Enable, BCD3, BCD2, BCD1, BCD0);
+module BCD_counter (Clock, Clear, Enable, count);
 
 input Clock, Clear, Enable;
-output wire [3:0] BCD3, BCD2, BCD1, BCD0;
+output reg [17:0] count;
 
-wire [4:1] Carry;
+always @(posedge Clock)
+begin
+	if (Clear)
+		count <= 0;
+	else if (Enable)
+	begin
+		count <= count + 1;
+	end
+end
 
-BCD_stage stage0 (Clock, Clear, Enable, BCD0, Carry[1]);
-BCD_stage stage1 (Clock, Clear, (Carry[1] & Enable), BCD1, Carry[2]);
-BCD_stage stage2 (Clock, Clear, (Carry[2] & Carry[1] & Enable), BCD2, Carry[3]);
-BCD_stage stage3 (Clock, Clear, (Carry[3] & Carry[2] & Carry[1] & Enable), BCD3, Carry[4]);
+//wire [4:1] Carry;
+//
+//BCD_stage stage0 (Clock, Clear, Enable, BCD0, Carry[1]);
+//BCD_stage stage1 (Clock, Clear, (Carry[1] & Enable), BCD1, Carry[2]);
+//BCD_stage stage2 (Clock, Clear, (Carry[2] & Carry[1] & Enable), BCD2, Carry[3]);
+//BCD_stage stage3 (Clock, Clear, (Carry[3] & Carry[2] & Carry[1] & Enable), BCD3, Carry[4]);
 
 endmodule
 
--- hundredth.v
+++ hundredth.v
@@ -7,10 +7,10 @@ reg [19:0] count_500k;
 
 always @(posedge Clock)
 	if (Load)
-		count_500k <= 20'h7A120;
+		count_500k <= 16'hC350;
 	else
 		count_500k <= count_500k - 1;
 	
-assign pulse_500k = (count_500k == 20'h00000);
+assign pulse_500k = (count_500k == 16'h0000);
 
 endmodule
